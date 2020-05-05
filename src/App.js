d99513b9eddac970d66ee0cc3c43492f136d768a
Onur Seckin Senoglu <seckin.senoglu@gmail.com>
2020-05-05 13:20:12 +0000

    Stage and Tetrominos are designed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Tetris from './components/Tetris'
+
+
+const App = () => {
+    return (
+        <div>
+        <Tetris />
+        </div>
+    )
+}
+
+
+export default App
---
+++ src/components/StartButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StartButton = ({callback}) => {
+    return (
+        <div>
+            Start Game
+        </div>
+    )
+}
+
+export default StartButton
